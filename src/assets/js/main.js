168d73de6793fffb99d3acee5871e86e96105a61
ahmedWessamTest <ahmedwessam202@gmail.com>
2025-09-30 17:17:58 +0200

    initial commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+// const heroSwiper = new Swiper(".heroSwiper", {
+//       loop: true,
+//       autoplay: { delay: 5000 },
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//       effect: "fade",
+//     });
